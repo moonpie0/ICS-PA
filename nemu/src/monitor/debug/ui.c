1d82df13c5885adedf1f82fd972db29b3d4eca71
tracer-ics2017 <tracer@njuics.org>
2024-03-03 01:40:05 -0800

    >  compile 2110939 ly Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 athlon i686 GNU/Linux  01:40:05 up  4:27,  1 user,  load average: 0.72, 0.48, 0.42 e8d4b54475750f9d33c1f64ff386014f3b17880e

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -38,6 +38,17 @@ static int cmd_q(char *args) {
 
 static int cmd_help(char *args);
 
+static int cmd_si(char *args){
+    /* extract the first argument */
+  char *arg = strtok(NULL, " ");
+
+  if(arg!=NULL)
+    cpu_exec(atoi(arg));
+  else  
+    cpu_exec(1);
+  return 0;
+}
+
 static struct {
   char *name;
   char *description;
@@ -48,6 +59,8 @@ static struct {
   { "q", "Exit NEMU", cmd_q },
 
   /* TODO: Add more commands */
+  {"si", "Execute the N instructions step by step", cmd_si},
+
 
 };
 
