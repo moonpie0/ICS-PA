b8a00c47c0da51dab522f4c75aefdcd7274ec0d8
tracer-ics2017 <tracer@njuics.org>
2024-03-28 01:14:18 -0700

    >  compile 2110939 ly Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  01:14:18 up  5:15,  1 user,  load average: 0.30, 0.17, 0.20 d4ccf1ba33547f8f49ba81416ff51b597e11ff4b

--- nemu/src/cpu/reg.c
+++ nemu/src/cpu/reg.c
@@ -18,7 +18,7 @@ void reg_test() {
   for (i = R_EAX; i <= R_EDI; i ++) {
     sample[i] = rand();
     reg_l(i) = sample[i];
-    printf("reg_w(i)%d==sample%d",reg_w(i),sample[i] & 0xffff);
+    //printf("reg_w(i)%d==sample%d",reg_w(i),sample[i] & 0xffff);
     assert(reg_w(i) == (sample[i] & 0xffff));
   }
 
