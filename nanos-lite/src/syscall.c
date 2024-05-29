6b0e75ab75a36c5df06c4210df6ad97732687675
tracer-ics2017 <tracer@njuics.org>
2024-05-28 20:21:39 -0700

    >  run 2110939 ly Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  20:21:39 up 1 day,  9:11,  1 user,  load average: 0.31, 0.13, 0.03 793e8cdca670dffd33d6199dde688b27dd612be8

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -31,7 +31,9 @@ int sys_write(int fd, void *buf, size_t len) {
 }
 
 int sys_brk(int addr) {
-  return 0;
+  extern int mm_brk(uint32_t new_brk);
+  return mm_brk(addr);
+  //return 0;
 }
 
 int sys_open(const char *pathname){
