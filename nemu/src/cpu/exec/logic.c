7114c35429f3ff10bc2312507d45a0b1e4958d18
tracer-ics2017 <tracer@njuics.org>
2024-06-09 08:40:50 -0700

    >  compile 2110939 ly Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  08:40:50 up 3 days,  9:46,  1 user,  load average: 0.19, 0.39, 0.36 45c0c3e6cf31340437a41d39c267e6a8dd6a168

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -104,3 +104,23 @@ make_EHelper(rol) {
 
   print_asm_template2(rol);
 }
+
+make_EHelper(ror){
+    rtl_li(&t0,31);
+	rtl_and(&id_src->val,&t0,&id_src->val);
+    rtl_li(&t1,(8*id_dest->width)-id_src->val);
+    rtl_shr(&t2,&id_dest->val,&id_src->val);
+    rtl_shl(&t3,&id_dest->val,&t1);
+	rtl_or(&t2,&t2,&t3);
+
+	if(id_src->val==1){
+	  rtl_msb(&t3,&t2,id_dest->width);
+	  rtl_shl(&t1,&t2,&id_src->val);
+	  rtl_msb(&t1,&t1,id_dest->width);
+	  rtl_xor(&t1,&t1,&t3);
+	  rtl_set_OF(&t1);
+	}
+
+	operand_write(id_dest,&t2);
+		print_asm_template2(ror);
+};
